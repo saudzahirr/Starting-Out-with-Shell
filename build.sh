2db7711deeabd33fab131f7114feb38438d3b8d8
saudz <saud.zahir@soco-engineers.com>
2024-04-15 17:43:16 +0500

    Fix build

--- build.sh
+++ build.sh
@@ -3,11 +3,15 @@
 PROJECT_NAME=$(grep -m1 "project(" CMakeLists.txt | sed 's/project(//' | sed 's/)//' | cut -d ' ' -f 1)
 EXECUTABLE=$(grep -m1 "add_executable(" CMakeLists.txt | sed 's/add_executable(//' | cut -d ' ' -f 1)
 
-echo "Building $PROJECT_NAME ..."
+echo "================================"
+echo "Building project $PROJECT_NAME ..."
 
 rm -rf build
 mkdir build
 cd build
+echo "================================"
+cmake --version
+echo "================================"
 cmake ..
 make
 
